84d5ea50bc46be30967f9b6450ecb1b3c7d6605f
Scott Renz <scottrenz@scottrenz.com>
2019-07-13 19:14:51 -0400

    made click events on tabs to show news head lines for the topics

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,58 +18,38 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 
-
 async function getData() {
-    let wait;
-  wait = await axios.get('https://lambda-times-backend.herokuapp.com/articles')
-  .then (data =>   
-      {
-      makeArts(data.data.articles)
-     return wait;}
-  
-  )
-  
-    .catch (data => { 
-      console.log('data error',data)
-            }
-             )
-    }
-    getData();
-
-
-  tops = document.querySelectorAll('.title')
+  let wait;
+wait = await axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then (data =>  { makeArts(data.data.articles)
+   return wait;} )
+ .catch (data => { console.log('data error',data)})}
+ getData();
 
   function makeArts(articles) {
-artKeys = Object.keys(articles)
-artEntries =  Object.entries(articles)
-
+    let artKeys = Object.keys(articles)
+    let artEntries =  Object.entries(articles)
 const cards = document.querySelectorAll('.cards-container');
-
-  for(i=0;i<artKeys.length;i++)
- {
-    for(j=0;j<artEntries[i][1].length;j++)
-    {
-        let divCard = document.createElement('div')
-        let divHead = document.createElement('div')
-        let divAuth = document.createElement('div')
-        let divImg = document.createElement('div')
-        let img = document.createElement('img')
-        let span = document.createElement('span')
-        divCard.setAttribute('class','card')
-        divHead.setAttribute('class','headline')
-        divImg.setAttribute('class','img-container')
-        divHead.innerHTML = artEntries[i][1][j]['headline']
-        img.src = artEntries[i][1][j]['authorPhoto']
-        span.innerHTML = 'By ' + artEntries[i][1][j]['authorName']
-        cards[0].appendChild(divCard)
-        divCard.appendChild(divHead)
-        divCard.appendChild(divAuth)
-        divAuth.appendChild(divImg)
-        divImg.appendChild(img)
-        divAuth.appendChild(span)
-        img.style = 'height: 50px;'
-        
-    }
- }
-
- }
+for(i=0;i<artKeys.length;i++)
+{
+  for(j=0;j<artEntries[i][1].length;j++)
+  {
+    let divCard = document.createElement('div')
+      let divHead = document.createElement('div')
+      let divAuth = document.createElement('div')
+      let divImg = document.createElement('div')
+      let img = document.createElement('img')
+      let span = document.createElement('span')
+      divCard.setAttribute('class','card')
+      divHead.setAttribute('class','headline')
+      divImg.setAttribute('class','img-container')
+      divHead.innerHTML = artEntries[i][1][j]['headline']
+      img.src = artEntries[i][1][j]['authorPhoto']
+      span.innerHTML = 'By ' + artEntries[i][1][j]['authorName']
+      cards[0].appendChild(divCard)
+      divCard.appendChild(divHead)
+      divCard.appendChild(divAuth)
+      divAuth.appendChild(divImg)
+      divImg.appendChild(img)
+      divAuth.appendChild(span)
+      img.style = 'height: 50px;'}}}
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -15,9 +15,7 @@ async function getData() {
       {
      makeTabs(data.data.topics)
      return wait;}
-  
   )
-  
     .catch (data => { 
       console.log('data error',data)
             }
@@ -26,18 +24,57 @@ async function getData() {
   getData();
 
   tops = document.querySelectorAll('.title')
+  let tabs=[];
 
   function makeTabs(topics) {
- let tabs=[];
-
+ 
  for(i=0;i<topics.length;i++)
  {
      tabs[i] = document.createElement('div')
-     tabs[i].setAttribute('data-tab',topics[i])
      tabs[i].classList.add('tab')
      tabs[i].innerHTML = topics[i].toUpperCase() 
+     tabs[i].addEventListener('click', function() { getDataX.call(this);}, false);
      tops[0].appendChild(tabs[i])
  }
 
  } 
 tops[0].style = 'width: 10%;display: flex;justify-content: space-between;align-items: center;padding: 1.0rem;height: 5.8rem;'
+let topicName = ''
+  
+   function getDataX() {
+let tpcx = this.innerHTML.toLowerCase()
+let tpc = ''
+for (i=0;i<tpcx.length;i++)
+{
+  if (tpcx[i] === ' ' || tpcx[i] === '.')
+  {i = tpcx.length}
+  else
+{tpc = tpc + tpcx[i] }     
+ }
+    axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then (data =>  { 
+      makeArtsX(data.data.articles,tpc)
+      } )
+     .catch (data => { console.log('data error',data)})}
+    
+   function   makeArtsX(articles,tpc) {
+    let k = 0;
+    let artKeys = Object.keys(articles)
+    let artEntries =  Object.entries(articles)
+
+    const cards = document.querySelectorAll('.cards-container');
+    xxx = [{}];
+    for(i=0;i<artKeys.length;i++)
+    {
+      for(j=0;j<artEntries[i][1].length;j++)
+      {
+    xxx[k] = {topic: artEntries[i][0], headline: artEntries[i][1][j]['headline']}
+    k = k + 1
+        }}
+let headlines = xxx.filter(obj => obj.topic === tpc);
+let headlineX = '';
+headlines.forEach(function(item) {
+  headlineX = headlineX + item.headline + "\r \n"
+})
+alert(headlineX)
+   } 
